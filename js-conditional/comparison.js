b4ba5eacd16541a76e5798df0cbf62190a60c0b4
Md Rabby <ablazerabbi159@gmail.com>
2024-04-19 01:18:07 +0600

    comparison related code

--- js-conditional/comparison.js
+++ js-conditional/comparison.js
@@ -1,1 +1,17 @@
-console.log(5 < 10);
+// console.log(10 == 10);
+// console.log(10 >= 10);
+// console.log(8 <= 10);
+// console.log(50 != 54);
+// console.log(50 < 54);
+// console.log(50 > 54);
+
+let a = 25;
+let b = 16;
+
+console.log(a > b); //a is greater then b
+console.log(a < b); //a is less then b
+console.log(a >= b); //a is greater then or equal b
+console.log(a <= b); //a is less then or equal b
+console.log(a == b); //a is equal b
+console.log(a != b); //a is not equal b
+console.log(a === '25'); //when we use '===' thats means this condition check also data type.
