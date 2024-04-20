93c24ad34125087c820976c51a08da3ce5e71f6d
Md Rabby <ablazerabbi159@gmail.com>
2024-04-21 02:16:02 +0600

    loop

---
+++ loop/for.js
@@ -0,0 +1,14 @@
+// for (initialization; condition; increment/decrement) {
+//     // Code to be executed in each iteration
+// }
+
+// let a = 5; //loop variable 
+
+// while(a <10){ //loop condition
+//     console.log(a); //loop statement
+//     a++; //loop variable change
+// }
+
+for(let a = 5; a < 10; a++){
+    console.log(a);
+}
---
+++ loop/while.js
@@ -0,0 +1,6 @@
+let a = 5; //loop variable 
+
+while(a <10){ //loop condition
+    console.log(a); //loop statement
+    a++; //loop variable change
+}
