b889d539deccb4252b9666ca34cf3693bf40445b
Md Rabby <ablazerabbi159@gmail.com>
2024-04-21 00:11:19 +0600

    tenary update

--- js-conditional/ternary.js
+++ js-conditional/ternary.js
@@ -1,8 +1,42 @@
-const age = 10;
+// condition ? expressionIfTrue : expressionIfFalse;
 
-if( age > 18){
-    console.log('Go to vote');
-}
-else{
-    console.log('if you have political brother then you also can vote');
-}
+
+// const age = 10;
+
+// if( age > 18){
+//     console.log('Go to vote');
+// }
+// else{
+//     console.log('if you have political brother then you also can vote');
+// }
+
+//-------------------ternary--------------------
+// const age = 20;
+// age > 18 ? console.log('Go to vote') : console.log('gumao jao');
+//-------------------ternary--------------------
+
+
+// let price = 500;
+// const isOwner = true;
+
+// if(isOwner === true){
+//     if(price > 300){
+//         price = 0;
+//     }
+//     else{
+//         price;
+//     }
+// }
+// else{
+//     price = price + 200;
+// }
+
+// console.log(price);
+
+//-------------------ternary--------------------
+let price = 800;
+const isOwner = true;
+
+price = isOwner === true ? price > 700 ? 0 : price : price + 200;
+
+console.log(price);
