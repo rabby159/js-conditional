7ac3ffd483b086fd25619348e148bf10ea426b55
Md Rabby <ablazerabbi159@gmail.com>
2024-04-21 12:35:29 +0600

    string

---
+++ string.js/string.js
@@ -0,0 +1,41 @@
+//toLowerCase
+//toUpperCase 
+//trim
+//string
+//when we compare two string
+
+// const subject = 'Physics';
+// const book = 'physics';
+
+// if(subject === book){
+//     console.log('Subject and book are same!');
+// }
+// else{
+//     console.log('Subject and book are not same!');
+// }
+
+
+// if(subject.toLowerCase() === book.toLowerCase()){
+//     console.log('Subject and book are same');
+// }
+// else{
+//     console.log('Subject and book are not same!');
+// }
+
+
+const tissue = 'fresh';
+const water = 'fresh ';
+
+// if(tissue === water){
+//     console.log('Tissue and water are same brand!');
+// }
+// else{
+//     console.log('Tissue and water are not same brand!');
+// }
+
+if(tissue.trim() === water.trim()){
+    console.log('Tissue and water are same brand!');
+}
+else{
+    console.log('Tissue and water are not same brand!');
+}
