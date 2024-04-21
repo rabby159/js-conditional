60b58261606151ad3b1467200e9d05482690cbdc
Md Rabby <ablazerabbi159@gmail.com>
2024-04-22 01:20:27 +0600

    set value

--- object/object.js
+++ object/object.js
@@ -1,8 +1,18 @@
 //object 
 
+
+//non primitive
 const watch = {
     brand: 'apple',
     price: 25000,
     color: "Blue",
     type: 'smart'
 }
+
+//dot notation
+// console.log(watch.brand);
+// console.log(watch['price']);
+
+
+watch.color = 'black'; 
+console.log(watch.color); //outpur: black
