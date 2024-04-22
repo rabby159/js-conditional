811945239e75b609f84ff5077946401e7cbab5e2
Md Rabby <ablazerabbi159@gmail.com>
2024-04-22 23:13:15 +0600

    function

---
+++ function/function.js
@@ -0,0 +1,15 @@
+//structure
+
+// function functionName(parameter1, parameter2, ...) {
+//     // Function body: code block that defines what the function does
+//     // It can contain any valid JavaScript statements
+//     // Optionally, the function can return a value using the return statement
+//   
+
+function sum(a, b){
+   total =  a + b;
+    return total;
+}
+
+let result = sum(5, 7);
+console.log(result);
