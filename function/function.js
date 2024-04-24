fcb2da2efdbb7b00d2bf005e6da6099196a2e272
Md Rabby <ablazerabbi159@gmail.com>
2024-04-25 04:21:26 +0600

    funciton problem1

--- function/function.js
+++ function/function.js
@@ -23,12 +23,34 @@
 
 //string parameter function
 
-function strParams(str){
-    const name = str;
-    const size = str.length;
+// function strParams(str){
+//     const name = str;
+//     const size = str.length;
 
-    console.log(name, size);
-};
+//     console.log(name, size);
+// };
 
-const result = strParams('Rabby');
-// console.log(result);
+// const result = strParams('Rabby');
+// // console.log(result);
+
+//Problem1: Write a function to give me the sum of all numbers in an array.
+/**
+ * step1: declare function
+ * step2: call function properly
+ * step3: declare parameter
+ * step4: check task step by step
+ */
+
+
+function sumOfAllNumber(arrNumbers){
+    let arrSum = 0;
+    for(const arrNumber of arrNumbers){
+        // console.log(arrNumbs);
+        arrSum = arrSum + arrNumber;
+    }
+    return arrSum;
+}
+
+const arr = [2, 1, 3, 9, 0, 2, 1, 2, 4];
+const totalSum = sumOfAllNumber(arr);
+console.log('Total sum of you ID: ',totalSum);
