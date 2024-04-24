7ad764862fc4198faae8f904c853a11f6b3d0565
Md Rabby <ablazerabbi159@gmail.com>
2024-04-25 03:46:05 +0600

    different params

--- function/function.js
+++ function/function.js
@@ -6,10 +6,29 @@
 //     // Optionally, the function can return a value using the return statement
 //   
 
-function sum(a, b){
-   total =  a + b;
-    return total;
-}
+// function sum(a, b){
+//    total =  a + b;
+//     return total;
+// }
 
-let result = sum(5, 7);
-console.log(result);
+// let result = sum(5, 7);
+// console.log(result);
+
+// function details(){
+//     console.log('Okey');
+//     console.log('Not Okey');
+// };
+
+// details();
+
+//string parameter function
+
+function strParams(str){
+    const name = str;
+    const size = str.length;
+
+    console.log(name, size);
+};
+
+const result = strParams('Rabby');
+// console.log(result);
