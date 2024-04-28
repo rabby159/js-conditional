dcde50ca4755ffe24d89c83cc0c2d0e20ec7060a
Md Rabby <ablazerabbi159@gmail.com>
2024-04-29 00:37:32 +0600

    spread and rest operator js

--- speedOperator/speedOp.js
+++ speedOperator/speedOp.js
@@ -1,11 +1,23 @@
 
 
-const myArray = [1, 2, 3];
+// const myArray = [1, 2, 3];
 
-const myCopyArray = [...myArray]; //[1, 2, 3]
+// const myCopyArray = [...myArray]; //[1, 2, 3]
 
-myArray.push(4);
-myCopyArray.push(5);
+// myArray.push(4);
+// myCopyArray.push(5);
 
-console.log(myArray);
-console.log(myCopyArray);
+// console.log(myArray);  //[1, 2, 3]
+// console.log(myCopyArray); //[1, 2, 3]
+
+
+const myRestOp = (...rest) => {
+    // console.log(rest);
+    let sum = 0;
+    rest.forEach((num) => {
+        sum = sum + num;
+    });
+    console.log(sum);
+};
+
+myRestOp(2, 1, 4, 5, 8);
